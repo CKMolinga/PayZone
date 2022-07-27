9a73eb024d279501d4a1c3651040b29ce6a19b56
CKMolinga <ck.molinga@gmail.com>
2022-07-27 13:32:13 +0200

    maintainance page

--- src/components/ConfirmPayment.js
+++ src/components/ConfirmPayment.js
@@ -42,6 +42,10 @@ const ConfirmPayment = () => {
     } catch (error) {
       console.log(error);
       setIsLoading(false);
+
+      if (error.code === "ERR_NETWORK") {
+        history.push("/maintainance");
+      }
     }
   };
 
