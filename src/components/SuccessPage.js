aff953687e8fd5d8d61e28a735e0836d0d9c3ee7
CKMolinga <ck.molinga@gmail.com>
2022-07-27 13:52:53 +0200

    Payzone version 1.0.0

--- src/components/SuccessPage.js
+++ src/components/SuccessPage.js
@@ -1,22 +1,18 @@
 import "../assets/styles/MakePayment.css";
 import { Link } from "react-router-dom";
 
-// import axios from "axios";
-// import { useState } from "react";
-
 import PaymentSuccesful from "../assets/payment-successful.gif";
 
 const SuccessPage = () => {
+  const amount = localStorage.getItem("total_amount");
+
   return (
     <section className="container">
       <div className="form-container">
         <form className="transaction">
           <img src={PaymentSuccesful} className="PaymentSuccesful-img" alt="" />
           <p className="payment-description">
-            {" "}
-            Sucessful payment of{" "}
-            <span className="green">{localStorage.getItem("amount")}</span> XAF
-            to
+            Sucessful payment of {amount} XAF
           </p>
 
           <div className="form-group go-to-home">
